4d3c3a7a3899c4c51a2e02b0b1f27bfaf3cdb1ab
MASHOD0 <mashhood2002@gmail.com>
2021-04-19 00:28:52 +0530

    added username and password

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+#!/usr/bin/activate python
+# encoding: utf-8
+
+
 import psycopg2
 import hashlib
+import getpass
 
 def main():
-
-
+    # taking the initial inputs
+    user = input("Username:")
+    password = getpass.getpass(prompt="Password:", stream=None)
+    
 
 
 
